40a9118ead07024772ead5f43ef215bf3ae217fd
Johna <johnathan@geeklys.com>
2021-08-22 07:00:36 +0900

    Amazon Pay donation

--- lambda-functions/save-to-blockchain/src/route/index.ts
+++ lambda-functions/save-to-blockchain/src/route/index.ts
@@ -1,15 +1,20 @@
 import {Router} from "express";
 import web3, {admin} from "../utils/GetWeb3";
-import SimpleStorage from "../contracts/SimpleStorage.json";
+import AmazonPayDonation from "../contracts/AmazonPayDonation.json";
 import {AbiItem} from "web3-utils";
 
-const simpleStorageContract = new web3.eth.Contract(SimpleStorage.abi as AbiItem[], SimpleStorage.networks["3"].address);
+const networkId = "3";
+const amazonPayDonationContract = new web3.eth.Contract(
+  AmazonPayDonation.abi as AbiItem[],
+  AmazonPayDonation.networks[networkId].address
+);
 
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const num = await simpleStorageContract.methods.get().call();
-  const tx = await simpleStorageContract.methods.set(Number(num) + 1);
+  const tx = await amazonPayDonationContract.methods.saveDonation(
+    ["johna","P03-0000-0000", 1500]
+  );
   const [gasPrice, gas] = await Promise.all([
     web3.eth.getGasPrice(),
     tx.estimateGas({
@@ -30,13 +35,6 @@ router.get("/", async (req, res) => {
         message: transactionHash
       })
     });
-});
-
-router.get("/get", async (req, res) => {
-  const num = await simpleStorageContract.methods.get().call();
-  return res.json({
-    message: num
-  })
 });
 
 export default router;
