a216ac17738e1013feeaa292b2ba7d4179897d57
Johna <johnathan@geeklys.com>
2021-08-25 02:52:56 +0900

    add backend for twitter

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import Twit from "twit";
+import axios from "axios";
+
+dotenv.config({path: "../.env"});
+
+const T = new Twit({
+  consumer_key: process.env.TWITER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+const stream = T.stream('statuses/filter', {track:'#TESTTESTJOHNA1203'});
+
+stream.on('connect', () => {
+  console.log('connect');
+});
+
+stream.on('tweet', (tweet) => {
+  axios.post(
+    `${process.env.API_SERVER_URL}/sqs`,
+    {
+      "accessToken": "dummy",
+      "oroId": "P03-XXXXXXX-XXXXXX",
+      "userId": tweet.user.screen_name,
+      "amount": 1000
+    }
+  ).then((result) => console.log(result.data));
+});
